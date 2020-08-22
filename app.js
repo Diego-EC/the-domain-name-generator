9be08471abf99aa936bf49e049c8e45d0b78332a
Diego-EC <diegoezquerro@gmail.com>
2020-08-22 10:59:47 +0000

    Changed function name and improved code.
    Signed-off-by: Diego-EC <diegoezquerro@gmail.com>

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 // declare the arrays with the words to combine
 const pronoun = ['the','our'];
-const adj = ['great', 'big' ];
+const adj = ['great', 'big', 'perfect'];
 const noun = ['jogger','racoon'];
 
 /* function invoke */
-generateRandomDomainName();
+generateDomainName();
 
 /* function implementation */
-function  generateRandomDomainName() {
-    console.log("lol");
-    for (let index = 0; index < pronoun.length; index++) {
+function  generateDomainName() {
+    for (let i = 0; i < pronoun.length; i++) {
+        const currentPronoun = pronoun[i];
         for (let j = 0; j < adj.length; j++) {
+            const currentAdj = adj[j];
             for (let k = 0; k < noun.length; k++) {
-                console.log(pronoun[index] + adj[j] + noun[k]+'.com'); 
+                const currentNoun = noun[k];
+                console.log(currentPronoun + currentAdj + currentNoun + '.com'); 
             }
         }
+    }
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
     }
 }
 
